4f50c433fbe132439681e0d5d9f8c128fc42458b
dserna <dserna@DanielSerna.maquila.local>
2015-06-23 14:16:10 -0500

    Subir proyecto

---
+++ Prueba-master/src/boutiquetrapitos/Datos.java
@@ -0,0 +1,24 @@
+
+package boutiquetrapitos;
+
+/**
+* Constructor de la clase datos
+* @author Daniel Serna
+*/
+
+public class Datos {
+   /**
+    * Metodo Booleano para validar usuario
+    * @param usuario
+    * @param contraseña
+    * @return true = Verdadero e ingresa al aplicativo.
+    */
+    public boolean validarIngreso(String usuario, String contraseña){
+        if(usuario.equals("sa") && contraseña.equals("123")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }  
+} 
