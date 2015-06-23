c775632b5ceedbe22208beeaa21b48871d9e5fa2
user <user@user-PC>
2015-06-23 15:21:55 -0500

    Revert "cambio..."
    This reverts commit 09c718eb9aedf74eae7832551628c34abbc6e075.

--- src/boutiquetrapitos/ClasePersonal.java
+++ src/boutiquetrapitos/ClasePersonal.java
@@ -3,7 +3,7 @@ package boutiquetrapitos;
 *Clase Persona de los metodos privados...
 * @author Daniel Serna
 * @version 23-06-2015
-* hola..
+* @param carro.
 */
 public class ClasePersonal {
     private String Documento;
